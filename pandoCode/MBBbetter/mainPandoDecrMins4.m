4e4ff85bbec954da46be9f7a2843049c6116f75d
edduriez <49229638+edduriez@users.noreply.github.com>
2022-09-23 17:41:59 +0200

    Add files via upload

---
+++ pandoCode/MBBbetter/mainPandoDecrMins4.m
@@ -0,0 +1,165 @@
+parpool('local',24)
+density=0.01:0.0001:1;
+m=size(density,2);
+prob='MBB'
+complfilename=['complHRr3.txt'];
+complfileID=fopen(complfilename);
+compliance=textscan(complfileID,'%24.10f');
+compliance=reshape(compliance{1,1},5,m)';
+c0=compliance(:,2);
+complfilename=['complHRr3initdes2.txt'];
+complfileID=fopen(complfilename);
+compliance=textscan(complfileID,'%24.10f');
+compliance=reshape(compliance{1,1},5,m-2)';
+c2=compliance(:,1);
+c2=[c0(1);c2;c0(end)];
+complfilename=['complHRr3initdes3.txt'];
+complfileID=fopen(complfilename);
+compliance=textscan(complfileID,'%24.10f');
+compliance=reshape(compliance{1,1},5,m-2)';
+c1=compliance(:,1);
+c1=[c0(1);c1;c0(end)];
+minToNow=10e9;
+minToNowold=10e9;
+locmins=[];
+locminsold=[];
+locminC=[];
+loccon=[];
+locconold=[];
+locconC=[];
+regN=10;
+regmins=zeros(1,10);
+regminsold=zeros(1,10);
+for i=1:regN
+    regspace=round(m/regN);
+    [regminC,regmini]=min(density(regspace*(i-1)+1:regspace*i)'.*c1(regspace*(i-1)+1:regspace*i));
+    regmins(i)=regmini+regspace*(i-1);
+    [regminC,regmini]=min(density(regspace*(i-1)+1:regspace*i)'.*c2(regspace*(i-1)+1:regspace*i));
+    regminsold(i)=regmini+regspace*(i-1);
+end
+for i=1:m-1
+    if c1(i)<minToNow && c1(i)<0.998*c1(i+1)
+        locmins=[locmins i];
+        locminC=[locminC c1(i)];
+        minToNow=c1(i);
+    end
+    if c2(i)<minToNowold && c2(i)<0.998*c2(i+1)
+        locminsold=[locminsold i];
+        minToNowold=c2(i);
+    end
+end
+for i=4:m
+    dif1=c1(i)-c1(i-1);
+    dif2=c1(i-1)-c1(i-2);
+    dif3=c1(i-2)-c1(i-3);
+%    if 5*dif2>dif1 %locally concave
+%    if dif2<0 && 5*dif2>dif1 %locally concave
+    if dif3<0 && dif2<0 && 4*(dif2+dif3)/2>dif1  && c1(i)<0.998*c1(i-1) %locally highly concave
+        loccon=[loccon i];
+        locconC=[locconC c1(i)];
+    end
+    
+    dif1old=c2(i)-c2(i-1);
+    dif2old=c2(i-1)-c2(i-2);
+    dif3old=c2(i-2)-c2(i-3);
+%    if 5*dif2>dif1 %locally concave
+%    if dif2<0 && 5*dif2>dif1 %locally concave
+    if dif3old<0 && dif2old<0 && 4*(dif2old+dif3old)/2>dif1old  && c2(i)<0.998*c2(i-1) %locally highly concave
+        locconold=[locconold i];
+    end
+end
+impPts=union(loccon,union(locmins,regmins));
+impPts=[1 impPts m];
+locmins=[1 locmins];
+loccon=[loccon m];
+
+impPtsold=union(locconold,union(locminsold,regminsold));
+impPtsold=[1 impPtsold m];
+locminsold=[1 locminsold];
+locconold=[locconold m];
+
+debug=2000
+%for i=2:size(impPts,2)
+parfor i=1:size(impPts,2)
+    i
+    if i==1 || i==size(impPts,2)
+        [v1,c2,v2,c3,v3,design5]=mainTop88allinit(200,100,density(impPts(i)),prob);
+    else
+        j=impPts(i);
+        %%%
+        %find closest local min with lower dens
+        locmindifold=locminsold-j;
+        closestLocMinold=find(locmindifold<0,1,'last');
+        num2str(locminsold(closestLocMinold))
+        desfilenameold=['initdes',num2str(locminsold(closestLocMinold))]
+        desfileIDold=fopen(desfilenameold);
+        designold=textscan(desfileIDold,'%24.10f');
+        initdes1old=reshape(designold{1,1},100,200);
+        [c1,v1,design1]=mainTop88oneinit(200,100,density(j),initdes1old,prob);
+
+
+        %find closest local concave
+        loccondifold=locconold-j;
+        closestLocconold=find(loccondifold>0,1);
+        num2str(locconold(closestLocconold))
+        desfilenameold=['initdes',num2str(locconold(closestLocconold))]
+        desfileIDold=fopen(desfilenameold);
+        designold=textscan(desfileIDold,'%24.10f');
+        initdes2old=reshape(designold{1,1},100,200);
+        [c2,v2,design2]=mainTop88oneinit(200,100,density(j),initdes2old,prob);
+        
+        %regmin 
+        regidx=floor(j/m*regN)+1;
+        desfilename3=['initdes',num2str(regminsold(regidx))];
+        desfileID3=fopen(desfilename3);
+        design3=textscan(desfileID3,'%24.10f');
+        initdes3=reshape(design3{1,1},100,200);
+        [c3,v3,design3]=mainTop88oneinit(200,100,density(j),initdes3,prob);
+        
+        
+        comp=[c1,c2,c3];
+        vf=[v1,v2,v3];
+        des=[design1;design2;design3];
+        [c5,mini]=min(comp);
+        vf5=vf(mini);
+        design5=des(mini,:);
+    end
+    locfilename=['initdes2_',num2str(impPts(i))];
+    locfileID=fopen(locfilename,'w');
+    fprintf(locfileID,'%14.10f',design5(:));
+end
+%%%
+%test following 
+    
+% parfor j =1:m
+%     %find closest local min with lower dens
+%     locmindif=locmins-j;
+%     closestLocMin=find(locmindif<0,1,'last');
+%     desfilename=['initdes',num2str(locmins(closestLocMin))];
+%     desfileID=fopen(desfilename);
+%     design=textscan(desfileID,'%24.10f');
+%     initdes1=reshape(design{1,1},100,200);
+%     [c1,v1]=mainTop88initdes(200,100,density(j),initdes1);
+%     
+%     
+%     %find closest local concave
+%     loccondif=loccon-j;
+%     closestLoccon=find(loccondif>0,1);
+%     desfilename=['initdes',num2str(loccon(closestLoccon))];
+%     desfileID=fopen(desfilename);
+%     design=textscan(desfileID,'%24.10f');
+%     initdes2=reshape(design{1,1},100,200);
+%     [c2,v2]=mainTop88initdes(200,100,density(j),initdes2);
+% 
+%     comp=[c1,c2];
+%     vf=[v1,v2];
+%     [c3,mini]=min(comp);
+%     vf3=vf(mini);
+%     locfilename=['pointHRr3initdes',num2str(j)];
+%     locfileID=fopen(locfilename,'w');
+%     fprintf(locfileID,'%14.10f %14.10f %14.10f %14.10f %14.10f',c3,c1,v1,c2,v2);
+% end
+    
+fclose('all');
+
+
---
+++ pandoCode/MBBbetter/readDataHRr3.m
@@ -0,0 +1,164 @@
+clear all
+density=0.01:0.0001:1;
+density=density';
+m=size(density,1);
+complfilename=['complHRr3.txt'];
+complfileID=fopen(complfilename);
+compliance=textscan(complfileID,'%24.10f');
+compliance=reshape(compliance{1,1},5,9901)';
+c1=compliance(:,2);
+
+complfilename=['complHRr3initdes6.txt'];
+complfileID=fopen(complfilename);
+compliance=textscan(complfileID,'%24.10f');
+compliance=reshape(compliance{1,1},5,9899)';
+c2=compliance(:,1);
+c2=[c1(1);c2;c1(end)];
+
+
+complfilename=['complHRr3initdes4.txt'];
+complfileID=fopen(complfilename);
+compliance=textscan(complfileID,'%24.10f');
+compliance=reshape(compliance{1,1},5,9899)';
+c0=compliance(:,1);
+c0=[c1(1);c0;c1(end)];
+% c3=compliance(:,2);
+% c3=[c1(1);c3;c1(end)];
+% c4=compliance(:,4);
+% c4=[c1(1);c4;c1(end)];
+
+
+%allmin=min(c1,c2);
+
+fclose('all')
+
+figure(1)
+plot(density,c1)
+% figure(2)
+% plot(density,c2)
+% figure(3)
+% plot(density,c1-c2)
+
+% figure(11)
+% plot(density,c3)
+% figure(12)
+% plot(density,c4)
+% figure(13)
+% plot(density,(c3-c4)./(c3+c4))
+% 
+figure(4)
+plot(density,c1)
+hold on
+plot(density,c2)
+% hold on
+% plot(density,c3)
+% hold on
+% plot(density,c4)
+%legend('11initdes','11initdes+backfront','locmins','loccon')
+legend('11initdes','11initdes+backfront')
+hold off
+
+
+%max(density-v0)
+%max(density-v1)
+%max(density-v2)
+%volfrac times compliance
+optim1=density.*c1;
+optim2=density.*c2;
+optim0=density.*c0;
+figure(5)
+plot(density,optim1)
+figure(6)
+plot(density,optim2)
+xlabel('volume fraction (V_f)');
+ylabel('V_f * f(V_f)');
+figure(8)
+plot(density,optim1)
+hold on
+plot(density,optim2)
+%xlim([0.16 0.22])
+legend('11initdes','11initdes+backfront')
+hold off
+
+figure(9)
+plot(density,optim1)
+hold on
+plot(density,optim2)
+hold on
+plot(density,optim0)
+%xlim([0.16 0.22])
+legend('11initdes','11initdes+2backfront','11initdes+backfront')
+hold off
+
+
+figure(18)
+plot(density,optim2)
+hold on
+plot(density,optimallmin)
+hold off
+legend('backfront','min old and backfront')
+
+%c=vf^n
+%filter first
+oFilt10P1 = filter((1/10)*ones(1,10),1,optimP1);
+% %gauss filter
+% n=11 %number of points
+% sig=n/8
+% xgauss=0:1:n-1
+% ygauss=1/(sig*sqrt(2*pi))*exp(-0.5*(xgauss-(n-1)/2).^2./sig^2);
+% oFiltGaussP1 = conv(optimP1,ygauss);
+figure(9)
+plot(density,oFilt10P1)
+% figure(10)
+% plot(density,oFiltGaussP1(1:end-10))
+
+window=[3,10,30,100,300,1000];
+%window=[10,100,1000];
+n=size(window,2)
+for i=1:n
+    %cP1=c01;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    win=window(i);
+    hwin=round(win/2);
+    cFilt=filter((1/win)*ones(1,win),1,cP1);
+    cFilt=cFilt(win:end);
+    densT=density(hwin:end-(win-hwin));
+    npower=(cFilt(2:end)-cFilt(1:end-1))./(densT(2)-densT(1)).*densT(1:end-1)./cFilt(1:end-1);
+    figure(20+i)
+    plot(densT(1:end-1),npower)
+    %figure(30+i)
+    %plot(densT,cFilt)
+    xgauss=0:1:win-1;
+    sig=win/8;
+    ygauss=1/(sig*sqrt(2*pi))*exp(-0.5*(xgauss-(win-1)/2).^2./sig^2);
+    cFiltG=conv(cP1,ygauss);
+    cFiltGT=cFiltG(win:end-win);
+    densTG=density(hwin+1:end-win+hwin);
+    %figure(40+i)
+    %plot(densTG,cFiltGT)
+    npowerG=(cFiltGT(2:end)-cFiltGT(1:end-1))./(densTG(2)-densTG(1)).*densTG(1:end-1)./cFiltGT(1:end-1);
+    figure(50+i)
+    plot(densTG(1:end-1),-npowerG)
+    hold on  %superimpose
+    nanal1=(3*densTG.^3-15*densTG.^2+24*densTG-24)./((4*densTG.^2-12*densTG+12).*(densTG-2));
+    plot(densTG(1:end-1),nanal1(1:end-1)) %superimpose
+    nanal2=(3-6*densTG+3*densTG.^2)./(3-3*densTG+densTG.^2);
+    plot(densTG(1:end-1),nanal2(1:end-1)) %superimpose
+    nanal3=(2*densTG-2)./(densTG-2);  %superimpose
+    plot(densTG(1:end-1),nanal3(1:end-1)) %superimpose
+    nanal4=5/3*densTG.*(1-densTG)./((1-densTG).^(1/3)-(1-densTG).^2);
+    plot(densTG(1:end-1),nanal4(1:end-1)) %superimpose
+    hold off %superimpose
+    ylim([0 2]);
+    xlabel('density')
+    ylabel('n')
+    legend('numerical','1st model','2nd model','3rd model','4th model')
+    
+    figure(60+i)
+    plot(densTG,cFiltGT.*densTG)
+    xlabel('volume fraction(V_f)')
+    ylabel('V_f \times f(V_f)')
+end
+
+
+
+fclose('all')
